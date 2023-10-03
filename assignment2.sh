4b4d70821084c1818f02a7e53f48640299f9d1d7
Muneerat <oladipomunirat@gmail.com>
2023-10-03 11:51:53 +0100

    add assignment2

---
+++ assignment2.sh
@@ -0,0 +1,35 @@
+<<'###END_COMMENT' 
+
+1. Directory Existence(-d):
+   use: To check if a directory exists.
+2. File Existence(-e) :
+   use: To check if a file exists.
+3. File permissions(-r,-w,-x)
+   use: To check file permissions(readable, writable,executable).
+4. Command substitution(`command` or $(command)):
+   use:To capture the output of a command and use it as a value
+5. Comparison Operators (-eq,-ne,-it,-le,-gt,-ge)
+6. (-b)
+    use: To check the file is a block special file or not
+7. (-s)
+   use: To check the file size is more then Zero
+8. (-n) 
+   use: To check the length of a string is non-zero or not
+9. (-c)
+   use: To check if the file is a character special file
+10. (-p)
+   use: To check whether the filename refers to a named pipe.
+11. (-u)
+   use: To check for setuid bit on files/directories.
+12. (-g)
+   use: To check for group id is set.
+13. (-k)
+   use: To check sticky bit is enabled in directories.
+14. (-o)
+use: To check ownership of the file.
+15. (-N)
+use: To check that the file has been modified since last read.
+#>>>>>End Comment<<<<<<<
+
+  
+###END_COMMENT
